ec952a812f916ad507b5b1f895e4756f71158ae6
bar1k4rea <b_andrey90@mail.ru>
2021-05-28 12:37:09 +0300

    hj

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 typedef struct Node {
     int key;
-    struct *Node left;
-    struct *Node right;
-    struct *Node parent;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
 } Node;
 
 
 void pre0rder(Node *ptr) {
     if (ptr == NULL)
         return;
-    printf(ptr->key);
+    printf("%d\n", ptr->key);
     pre0rder(ptr->left);
     pre0rder(ptr->right);
 }
@@ -18,12 +18,14 @@ void in0rder(Node *ptr) {
     if (ptr == NULL)
         return;
     pre0rder(ptr->left);
-    printf(ptr->key);
+    printf("%d\n", ptr->key);
     pre0rder(ptr->right);
 }
 
 void post0rder(Node *ptr) {
     if (ptr == NULL)
         return;
-
+    post0rder(ptr->left);
+    post0rder(ptr->right);
+    printf("%d\n", ptr->key);
 }
